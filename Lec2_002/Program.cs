615f2410b3ae5a1594610eeaaf8ffba063ae3e4a
Pavel <boytunov94@gmail.com>
2023-02-07 17:32:20 +0900

    Lec2 002

---
+++ Lec2_002/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Найти max из трех чисел (ФУНКЦИЯ)*/ 
+
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 12;
+int b1 = 15;
+int c1 = 122;
+int a2 = 23;
+int b2 = 2;
+int c2 = 1;
+int a3 = 145;
+int b3 = 4565;
+int c3 = 234;
+
+int max1 = Max (a1, b1, c1);
+int max2 = Max (a1, b1, c1);
+int max3 = Max (a1, b1, c1);
+int max = Max (max1, max2, max3);
+
+Console.WriteLine(max);
